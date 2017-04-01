c940c0c6f9f24b64ebf54f89e618a3f54a715567
Chris Sanders <sanders.chris@gmail.com>
2017-04-01 16:37:12 -0500

    Initial commit

---
+++ reactive/version.py
@@ -0,0 +1,8 @@
+from charms.reactive import when, when_not, set_state
+from charmhelpers.core.hookenv import application_version_set
+
+@when_not('layer-version.installed')
+def install_layer_version():
+    with open('VERSION','r') as version:
+        application_version_set(version.readline().strip())
+    set_state('layer-version.installed')
