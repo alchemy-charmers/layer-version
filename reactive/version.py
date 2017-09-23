ae702dd8a24b372378e8c1158a9a87b99f400599
Chris Sanders <sanders.chris@gmail.com>
2017-09-23 14:58:33 -0500

    Update version on all config changes to catch a charm-upgrade

--- reactive/version.py
+++ reactive/version.py
@@ -1,8 +1,15 @@
 from charms.reactive import when, when_not, set_state
 from charmhelpers.core.hookenv import application_version_set
 
+
 @when_not('layer-version.installed')
 def install_layer_version():
-    with open('VERSION','r') as version:
+    with open('VERSION', 'r') as version:
         application_version_set(version.readline().strip())
     set_state('layer-version.installed')
+
+
+@when('config.changed')
+def update_version():
+    with open('VERSION', 'r') as version:
+        application_version_set(version.readline().strip())
