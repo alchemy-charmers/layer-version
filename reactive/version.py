749b2a4b7014e645f698bd9428df839803480517
Chris Sanders <sanders.chris@gmail.com>
2019-01-11 17:08:33 -0600

    Made the version file name a layer configuration option

--- reactive/version.py
+++ reactive/version.py
@@ -1,15 +1,16 @@
 from charms.reactive import when_not, set_state, hook
 from charmhelpers.core.hookenv import application_version_set
+from charms import layer
 
 
 @when_not('layer-version.installed')
 def install_layer_version():
-    with open('VERSION', 'r') as version:
+    with open(layer.options('version')['file_name'], 'r') as version:
         application_version_set(version.readline().strip())
     set_state('layer-version.installed')
 
 
 @hook('upgrade-charm')
 def update_version():
-    with open('VERSION', 'r') as version:
+    with open(layer.options('version')['file_name'], 'r') as version:
         application_version_set(version.readline().strip())
